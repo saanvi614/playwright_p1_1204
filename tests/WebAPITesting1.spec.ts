0f1690872ec5380dac8279aeefb1f99402f83b54
swati jadhav <swaid2917@gmail.com>
2026-07-19 14:03:26 +0530

    removed credentials

--- tests/WebAPITesting1.spec.ts
+++ tests/WebAPITesting1.spec.ts
@@ -1,8 +1,9 @@
 import {test,expect,request} from '@playwright/test';
+import process from 'process';
 
 test('API Testing', async()=>{
 
-const loginPayload={userEmail: "swaid2917@gmail.com", userPassword: "mypass@2026ASDWW"};
+const loginPayload={userEmail: process.env.CLIENT_EMAIL, userPassword: process.env.CLIENT_PASSWORD};
 
 const apiContext=await request.newContext();
 const loginResponse=await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',
